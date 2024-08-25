3f5e4c084ffcdf051ece8ae0cdc3628cb2c2e8d8
linguosity <brandon@linguosity.ai>
2024-08-25 12:57:57 -0700

    changed URL redirects in supabase and googleauth

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,14 +1,19 @@
-import { createSupabaseServerClient } from "@/lib/utils/supabase/server-client";
+import { createSupabaseServerClient } from "../../../lib/utils/supabase/server-client";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
+
   const code = searchParams.get("code");
+
+  // if "next" is in param, use it in the redirect URL
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = createSupabaseServerClient();
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`);
     }
